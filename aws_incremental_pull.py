2dd9b725c2310c4d8c4dbbc08eb22472a9334084
Un-4g1v3n <78097292+Un-4g1v3n@users.noreply.github.com>
2026-07-11 23:04:57 -0700

    Update aws_incremental_pull.py

--- aws_incremental_pull.py
+++ aws_incremental_pull.py
@@ -240,6 +240,10 @@ STATE_ONEDRIVE_FOLDER = (
     "/Reliability/AWSDB/State"
 )
 
+ONEDRIVE_FOLDER = (
+    "/Reliability/AWSDB/{SITE_GROUP}"
+)
+
 # ==========================================================
 # FILE SETTINGS
 # ==========================================================
@@ -431,7 +435,7 @@ def download_existing_csvs(token, site_id):
         )
 
         remote_path = (
-            f"{STATE_ONEDRIVE_FOLDER}/{filename}"
+            f"{ONEDRIVE_FOLDER}/{filename}"
         )
 
         url = (
@@ -470,7 +474,7 @@ def upload_csv(token, local_path, delete_after=False):
     filename = Path(local_path).name
 
     remote_path = (
-        f"{STATE_ONEDRIVE_FOLDER}/{filename}"
+        f"{ONEDRIVE_FOLDER}/{filename}"
     )
 
     upload_url = (
